5630dfe72b480cc9e79d5fc23e2b2743548c27d8
carlosacasallasc <carloscasallas6@gmail.com>
2021-09-07 17:24:52 -0500

    Clase conexion y test de conexion a la BBDD

--- Proyecto_web/src/main/conexion_BD.java
+++ Proyecto_web/src/main/conexion_BD.java
@@ -1,22 +1,37 @@
 package main;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexion_BD {
 	
-		public static void main(String[] args) throws SQLException {
-			try {
-				
-				Class.forName("com.mysql.cj.jdbc.Driver");
-				
-				DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda_generica_grp_fullstack","root","Desarrollo2020**");
-				
-				System.out.print("Conexión OK");
-				
-			} catch (Exception e) {
-				System.out.print("Error al cargar el controlador");
-				e.printStackTrace();
-			}
+	private static final String Conector = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/tienda_generica_grp_fullstack";
+	private static final String Usuario = "root";
+	private static final String PWD = "Desarrollo2020**";
+	
+	
+	
+	public Connection Conectar() {
+		
+		Connection conexion = null;
+		
+		try {
+			Class.forName(Conector);
+			DriverManager.getConnection(URL,Usuario,PWD);
+			System.out.print("Conexión OK");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.print("Error al cargar el controlador");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.print("Error en la conexión");
+			e.printStackTrace();
 		}
-}
+		
+		return conexion;
+	}
+		
+	}
+
---
+++ Proyecto_web/src/main/testConexion.java
@@ -0,0 +1,8 @@
+package main;
+
+public class testConexion {
+	public static void main(String[] args) {
+		conexion_BD conexion = new conexion_BD();
+		conexion.Conectar();
+	}
+}
